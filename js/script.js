b82044bdd63e2b0ea0cdfdf9c862f9872268c442
Kent Toledo <kent.toledo@outlook.com>
2019-08-26 20:21:40 +0800

    updated images and changed countdown

--- js/script.js
+++ js/script.js
@@ -121,7 +121,7 @@
     });
 
     /*CountDown*/
-    $('.married_coundown').countdown({until: new Date("Feb 18, 2017 11:00:00")});
+    $('.married_coundown').countdown({until: new Date("Jun 01, 2020 11:00:00")});
 
     /*OWL Carousel in Our Story*/
     $(".story_wrapper").owlCarousel({
